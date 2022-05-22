cdefcf964dbd2da3502b64a3a597be389c3142d2
chaochin <angel253698@gmail.com>
2022-05-22 17:51:29 -0400

    Update ProjectBlog.jsx

--- src/components/ProjectBlog.jsx
+++ src/components/ProjectBlog.jsx
@@ -17,102 +17,136 @@ const ProjectBlog = () => {
             Here are some of my courses project and blog I work on!
           </p>
         </div>
-        <div className=" grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {/* Work 1*/}
+        <div className="project-grid grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Cognitively-Driven Design*/}
           {/* Grid Image*/}
           <div className="h-auto border-2 shadow-md shadow-[#E6E6E6] group container rounded-md  justify-center items-center mx-auto work-content-div">
-            <header className="relative">
-              {/* File icon*/}
-              <ul className="project-top flex w-[100%] items-center m-2 p-2 h-[30%]">
-                <li className="absolute left-2 top-1">
-                  <AiOutlineFolderOpen className="text-[#b2fefd]" size="2rem" />
-                </li>
-                <li className="absolute right-2 top-1">
-                  <AiFillGithub className="text-[#b2fefd]" size="2rem" />
-                </li>
-              </ul>
-            </header>
-            <div className="mx-2 p-2">
-              <h1 className="project-title m-2 text-3xl">Dear Data Redux</h1>
+            <div className="h-[80%]">
+              <header className="relative">
+                {/* File icon*/}
+                <ul className="project-top flex w-[100%] items-center m-2 p-2 h-[30%]">
+                  <li className="absolute left-2 top-1">
+                    <AiOutlineFolderOpen
+                      className="text-[#b2fefd]"
+                      size="2rem"
+                    />
+                  </li>
+                  <li className="absolute right-2 top-1">
+                    <AiFillGithub className="text-[#b2fefd]" size="2rem" />
+                  </li>
+                </ul>
+              </header>
+              <div className="mx-2 p-2">
+                <h1 className="project-title m-2 text-3xl">
+                  <a href="">Cognitively-Driven Design</a>
+                </h1>
 
-              <div className="project-discription m-2">
-                This is the place for discription
+                <div className="project-discription m-2">
+                  COMP790 Assignment-4 Cognitively-driven design is all about
+                  using concrete experimentally-derived insight into how people
+                  make sense of visualization to drive design.
+                </div>
               </div>
             </div>
-            <footer className="m-2 mb-4 text-[#959e9e] p-4 overflow-break-word">
-              <ul className="">
+            <footer className="my-2 h-[6em]">
+              <ul className="m-2 mb-4 text-[#959e9e] p-4 overflow-break-word">
                 <li className="px-1 float-left">HTML</li>
                 <li className="px-1 float-left">CSS</li>
                 <li className="px-1 float-left">JavaScript</li>
+                <li className="px-1 float-left">D3.js</li>
                 <li className="px-1 float-left">Firebase</li>
               </ul>
             </footer>
-
-            {/* Hover Effect */}
           </div>
 
-          {/* Work 1*/}
+          {/* CDesign Study Lite*/}
           {/* Grid Image*/}
           <div className="h-auto border-2 shadow-md shadow-[#E6E6E6] group container rounded-md  justify-center items-center mx-auto work-content-div">
-            <header className="relative">
-              {/* File icon*/}
-              <ul className="project-top flex w-[100%] items-center m-2 p-2 h-[30%]">
-                <li className="absolute left-2 top-1">
-                  <AiOutlineFolderOpen className="text-[#b2fefd]" size="2rem" />
-                </li>
-                <li className="absolute right-2 top-1">
-                  <AiFillGithub className="text-[#b2fefd]" size="2rem" />
-                </li>
-              </ul>
-            </header>
-            <div className="mx-2 p-2">
-              <h1 className="project-title m-2 text-3xl">Dear Data Redux</h1>
-              <div className="project-discription m-2">
-                This is the place for discription
+            <div className="h-[80%]">
+              <header className="relative">
+                {/* File icon*/}
+                <ul className="project-top flex w-[100%] items-center m-2 p-2 h-[30%]">
+                  <li className="absolute left-2 top-1">
+                    <AiOutlineFolderOpen
+                      className="text-[#b2fefd]"
+                      size="2rem"
+                    />
+                  </li>
+                  <li className="absolute right-2 top-1">
+                    <AiFillGithub className="text-[#b2fefd]" size="2rem" />
+                  </li>
+                </ul>
+              </header>
+              <div className="mx-2 p-2">
+                <h1 className="project-title m-2 text-3xl">
+                  <a href="">Design Study Lite</a>
+                </h1>
+
+                <div className="project-discription m-2">
+                  Design studies are intensive, longitudinal, collaborative
+                  design activities. While they're a powerful tool in your
+                  visualization development toolkit, they also take a bit more
+                  energy than is reasonable for a module-sized assignment. To
+                  get you some practice with the design study methodology,
+                  you'll conduct a miniature design study with yourselves as
+                  both the designer and stakeholder. Your goal will be to design
+                  a visualization tool to support the following scenario.
+                </div>
               </div>
             </div>
-            <footer className="m-2 mb-4 text-[#959e9e] p-4 overflow-break-word">
-              <ul className="">
-                <li className="px-1 float-left">HTML</li>
-                <li className="px-1 float-left">CSS</li>
-                <li className="px-1 float-left">Firebase</li>
-              </ul>
-            </footer>
-
-            {/* Hover Effect */}
-          </div>
-
-          {/* Work 1*/}
-          {/* Grid Image*/}
-          <div className="h-auto border-2 shadow-md shadow-[#E6E6E6] group container rounded-md  justify-center items-center mx-auto work-content-div">
-            <header className="relative">
-              {/* File icon*/}
-              <ul className="project-top flex w-[100%] items-center m-2 p-2 h-[30%]">
-                <li className="absolute left-2 top-1">
-                  <AiOutlineFolderOpen className="text-[#b2fefd]" size="2rem" />
-                </li>
-                <li className="absolute right-2 top-1">
-                  <AiFillGithub className="text-[#b2fefd]" size="2rem" />
-                </li>
-              </ul>
-            </header>
-            <div className="mx-2 p-2">
-              <h1 className="project-title m-2 text-3xl">Dear Data Redux</h1>
-
-              <div className="project-discription m-2">
-                This is the place for discription
-              </div>
-            </div>
-            <footer className="m-2 mb-4 text-[#959e9e] p-4 overflow-break-word">
-              <ul className="">
+            <footer className="my-2 h-[6em]">
+              <ul className="m-2 mb-4 text-[#959e9e] p-4 overflow-break-word">
                 <li className="px-1 float-left">HTML</li>
                 <li className="px-1 float-left">CSS</li>
                 <li className="px-1 float-left">JavaScript</li>
+                <li className="px-1 float-left">D3.js</li>
                 <li className="px-1 float-left">Firebase</li>
               </ul>
             </footer>
+          </div>
 
-            {/* Hover Effect */}
+          {/* Dear Data Redux*/}
+          {/* Grid Image*/}
+          <div className="h-auto border-2 shadow-md shadow-[#E6E6E6] group container rounded-md  justify-center items-center mx-auto work-content-div">
+            <div className="h-[80%]">
+              <header className="relative">
+                {/* File icon*/}
+                <ul className="project-top flex w-[100%] items-center m-2 p-2 h-[30%]">
+                  <li className="absolute left-2 top-1">
+                    <AiOutlineFolderOpen
+                      className="text-[#b2fefd]"
+                      size="2rem"
+                    />
+                  </li>
+                  <li className="absolute right-2 top-1">
+                    <AiFillGithub className="text-[#b2fefd]" size="2rem" />
+                  </li>
+                </ul>
+              </header>
+              <div className="mx-2 p-2">
+                <h1 className="project-title m-2 text-3xl">
+                  <a href="">Dear Data Redux</a>
+                </h1>
+
+                <div className="project-discription m-2">
+                  Data reflects the world around us. We generate data through
+                  each action we take and each decision we make. However, most
+                  of this data simply passes by and is never collected or used.
+                  In this first module assignment, we explore patterns in data
+                  about our daily lives and use different media to explore and
+                  make sense of this data.
+                </div>
+              </div>
+            </div>
+            <footer className="my-2 h-[6em]">
+              <ul className="m-2 mb-4 text-[#959e9e] p-4 overflow-break-word">
+                <li className="px-1 float-left">HTML</li>
+                <li className="px-1 float-left">CSS</li>
+                <li className="px-1 float-left">JavaScript</li>
+                <li className="px-1 float-left">D3.js</li>
+                <li className="px-1 float-left">Firebase</li>
+              </ul>
+            </footer>
           </div>
         </div>
       </div>
